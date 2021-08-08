bcade5a8c5fb2ee982d0733e7afb7a2afe650aac
Sunera Sachintha <sachisune@gmail.com>
2021-08-08 13:19:42 +0530

    resource file modifcations

--- app/Http/Resources/Category/CategoryResource.php
+++ app/Http/Resources/Category/CategoryResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Category;
 
+use App\Repositories\Contracts\CategoryRepositoryInterface;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CategoryResource extends JsonResource
@@ -14,10 +15,40 @@ class CategoryResource extends JsonResource
      */
     public function toArray($request)
     {
-        //dd($this);
-        return [
-            "categories" => $this->resource
-        ];
-        //return parent::toArray($request);
+        $resourceResponse = $this->resource;
+        //When resource returns a collection
+        if($resourceResponse instanceof \Illuminate\Database\Eloquent\Collection){
+            return [
+                "data" => $resourceResponse->map(function ($category) {
+                    $data = [];
+                    $data['id']             = $category['id'];
+                    $data['name']           = $category['category_name'];
+                    $data['description']    = $category['category_description'];
+                    $data['is_parent']      = $category['is_parent'];
+                    $data['parent_id']      = isset($category['parent']) ? $category['parent']['id'] : null;
+                    $data['parent']         = isset($category['parent']) ? $category['parent']['category_name'] : null;
+                    $data['status']         = $category['status'];
+                    $data['created_at']     = timeStampToDate($category['created_at']);
+                    $data['updated_at']     = timeStampToDate($category['updated_at']);
+                    return $data;
+                }),
+                "results_count" => $resourceResponse->count()
+            ];
+        }else{ //When resource returns a Model
+            $data = [];
+            $data['id']             = $resourceResponse['id'];
+            $data['name']           = $resourceResponse['category_name'];
+            $data['description']    = $resourceResponse['category_description'];
+            $data['is_parent']      = $resourceResponse['is_parent'];
+            $data['parent_id']      = isset($resourceResponse['parent']) ? $resourceResponse['parent']['id'] : null;
+            $data['parent']         = isset($resourceResponse['parent']) ? $resourceResponse['parent']['category_name'] : null;
+            $data['status']         = $resourceResponse['status'];
+            $data['created_at']     = timeStampToDate($resourceResponse['created_at']);
+            $data['updated_at']     = timeStampToDate($resourceResponse['updated_at']);
+
+            return [
+                "data" =>  $data
+            ];
+        }
     }
 }

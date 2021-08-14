0067cc07aa3b5b1670b670b5fdd53aa6dda01a0b
Sunera Sachintha <sachisune@gmail.com>
2021-08-14 12:32:04 +0530

    auth middleware registration

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,9 @@
 
 namespace App\Http;
 
+
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Passport\Http\Middleware\CheckClientCredentials;
 
 class Kernel extends HttpKernel
 {
@@ -63,5 +65,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        //'client' => CheckClientCredentials::class,
+        'role' => \App\Http\Middleware\CheckRole::class,
+        'ability' =>  \App\Http\Middleware\RoleCan::class,
     ];
 }
